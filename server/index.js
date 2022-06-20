173822cfd1468abe3537d2d52d868f013114df56
Ella Wren <ella.wren@hotmail.co.uk>
2022-06-20 15:22:15 +0100

    Set up back-end server

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const PORT = 3001;
+const app = express();
+
+function logger(req, res, next) {
+    const start = Date.now();
+    res.on("finish", () => {
+        const end = Date.now();
+        const diffSeconds = (end - start) / 1000;
+        console.log(`${req.method} ${req.url} Completed in ${diffSeconds} seconds`);
+    });
+    next();
+}
+app.use(logger);
+
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
