56edff1cd963347eda30b0a635a7218b3d51a276
Ella Wren <ella.wren@hotmail.co.uk>
2022-06-21 20:11:16 +0100

    Display location data and date/time

--- client/src/LeftPanel.js
+++ client/src/LeftPanel.js
@@ -1,10 +1,32 @@
 function LeftPanel(props) {
-    const { currentWeather, locationData } = props;
+    const { currentWeather, datetime, locationData, weatherUnits } = props;
     return (
         <div className="col-lg-3 bg-dark text-white">
-            <p>{locationData.city}</p>
-            <p>{locationData.region}</p>
-            <p>{locationData.country}</p>
+            <div className="mt-2">
+                <h1 className="display-2 text-center">Weather</h1>
+                <div className="card">
+                    <div className="card-header text-body">
+                        <div className="text-center">
+                            {locationData.city}, {locationData.country}
+                        </div>
+                        <div className="w-100 clearfix">
+                            <span className="float-start">
+                                {datetime.toLocaleString('en-GB', {
+                                    weekday: "long",
+                                    month: "long",
+                                    day: "numeric",
+                                })}
+                            </span>
+                            <span className="float-end">
+                                {datetime.toLocaleString('en-GB', {
+                                    hour: "numeric",
+                                    minute: "2-digit",
+                                })}
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 };
--- client/src/WeatherContainer.js
+++ client/src/WeatherContainer.js
@@ -1,14 +1,28 @@
+import React, { useState } from 'react';
 import LeftPanel from './LeftPanel';
 import RightPanel from './RightPanel';
 
 function WeatherContainer(props) {
     const { locationData, weatherData } = props;
 
+    const [ datetime, setDatetime ] = useState(new Date());
+
+    const units = weatherData.meta.units;
+    for (const item in units) {
+        if (units[item] === 'celsius') {
+            units[item] = '\u00B0C';
+        } else if (units[item] === 'degrees') {
+            units[item] = '\u00B0';
+        }
+    }
+
     return (
         <div className="row vh-100">
             <LeftPanel
                 locationData={locationData}
                 currentWeather={weatherData.timeseries[0]}
+                weatherUnits={units}
+                datetime={datetime}
             />
             <RightPanel
                 weatherData={weatherData}
