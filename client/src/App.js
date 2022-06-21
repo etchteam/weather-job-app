980cfcc06e47d282c6ffa9bfc9451664b81f88dc
Ella Wren <ella.wren@hotmail.co.uk>
2022-06-21 19:39:09 +0100

    Handle data in front end

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,43 @@
 import React, { useEffect, useState } from "react";
 
 function App() {
-  
-  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  const [locationData, setLocationData] = useState({});
+  const [weatherData, setWeatherData] = useState(null)
 
   useEffect(() => {
     fetch("/api")
       .then((res) => res.json())
-      .then((data) => setData(data.city));
+      .then((result) => {
+        console.log(result)
+        setIsLoaded(true);
+        setLocationData({
+          lat: result.lat,
+          lon: result.lon,
+          city: result.city,
+          region: result.region,
+          country: result.country,
+        })
+        setWeatherData(result.weather);
+      },
+      (error) => {
+        setIsLoaded(true);
+        setError(error);
+      });
   }, []);
 
-  return (
-    <div className="App">
-      <p>{!data ? "Loading..." : data}</p>
-    </div>
-  );
+  if (error) {
+    console.log(error);
+    return <div>Error</div>;
+  } else if (!isLoaded) {
+    return <div>Loading...</div>;
+  } else {
+    return (
+      <div>Data found</div>
+    );
+  }
 }
 
 export default App;
